1bd02a42427f0d6bba1eaf3d86323ed6f4a05cf6
princePandeyGitHub <princecode13@gmail.com>
2024-09-01 12:16:32 +0530

    Update script(wrote logic for loader , wrote about feels like and changes color as per temperature.js

--- script.js
+++ script.js
@@ -12,19 +12,38 @@ document.getElementById('get-weather').addEventListener('click', function() {
     const apiKey = 'c2c08467d56d57513607c7d9da7dfa24';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
+    // Check if the info area is empty before showing the loader
+    const infoArea = document.getElementById('info-area');
+    if (infoArea.innerText.trim() === '') {
+        document.getElementById('loader').style.display = 'block'; // Show the loader only if no info is displayed
+    }
+
 
     // Fetch the weather data from OpenWeatherMap API
     fetch(url)
         .then(response => {
+            document.getElementById('loader').style.display = 'none';
+            console.log(response);
             if (!response.ok) {
                 throw new Error('City not found');
             }
             return response.json();
         })
         .then(data => {
+            // change the UI as per temperature
+            const body = document.getElementById('body');
+            // apply cold colors
+            if(data.main.temp <= 20){
+                body.style.backgroundColor = '#9294e6';
+            }
+            //apply hot colors
+            if(data.main.temp >= 21){
+                body.style.backgroundColor = '#ed95c1';
+            }
+
             // Update the UI with the fetched data
             document.getElementById('city-name').innerText = `City: ${data.name}`;
-            document.getElementById('temp').innerText = `Temperature: ${data.main.temp} °C`;
+            document.getElementById('temp').innerText = `Temperature: ${data.main.temp} °C feels like ${data.main.feels_like} °C`;
             document.getElementById('weather').innerText = `Weather: ${data.weather[0].description}`;
         })
         .catch(error => {
